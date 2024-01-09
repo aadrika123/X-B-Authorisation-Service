e0171f33404a8b16a6a65ce4dd6f0e128420ae5c
mrinal9911 <mrinalk123456@gmail.com>
2024-01-09 11:02:56 +0530

    rolewise

--- app/Pipelines/User/SearchByRole.php
+++ app/Pipelines/User/SearchByRole.php
@@ -8,12 +8,12 @@ class SearchByRole
 {
     public function handle($request, Closure $next)
     {
-        if (!request()->has('role')) {
+        if (!request()->has('rolewise')) {
             return $next($request);
         }
         return $next($request)
             ->join('wf_roleusermaps', 'wf_roleusermaps.user_id', 'users.id')
             ->join('wf_roles', 'wf_roles.id', 'wf_roleusermaps.wf_role_id')
-            ->where('wf_roles.id',  request()->input('role'));
+            ->where('wf_roles.id',  request()->input('rolewise'));
     }
 }
